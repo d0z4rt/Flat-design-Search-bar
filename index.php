7ecaba069cb39e69a2ee98efac7b1ce5c71a1bf1
mxcmaxime <join.mxcmaxime@outlook.fr>
2015-04-04 21:28:41 +0300

    add files

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Searching for something</title>
+		<meta name="author" content="mxcmaxime" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<h1>Searching for something</h1>
+		<form id="search_form">
+			<div class="bar_contener">
+				<fieldset>
+					<input class="bar" type="texte" id="search" name="search" required />
+					<div class="send_cont" onclick="document.getElementById('search_form').submit();">
+						<div class="arrow_part_1"></div>
+						<div class="arrow_part_2"></div>
+						<div class="arrow_part_3"></div>
+					</div>
+				</fieldset>
+			</div>
+			<div class="tail_cont"><div class="tail"></div></div>
+		</form>
+		<p>Made from the original design of <a href="https://dribbble.com/shots/2004982-Searching-for-something" target="_blank">Ivan Radovic</a> and developed by <a href="http://mxcmaxime.net" target="_blank">mxcmaxime</a></p>
+	</body>
+</html>
